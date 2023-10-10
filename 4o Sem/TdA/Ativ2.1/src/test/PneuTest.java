2a7c17be64a1922171cc7f693e3ac325a0c26598
Andre Bauzil <andregbauzil@gmail.com>
2023-10-10 15:40:24 -0300

    Adição de métodos de teste separados para calibrar os pneus e girá-los com e sem sinto (Integ), além do @BeforeEach para instanciar o Carro antes de cada teste

--- 4o Sem/TdA/Ativ2.1/src/test/PneuTest.java
+++ 4o Sem/TdA/Ativ2.1/src/test/PneuTest.java
@@ -1,17 +1,40 @@
 package test;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import main.Pneu;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+import main.Carro;
+import main.Pneu;
+
 class PneuTest {
+    private Carro carro;
+    private String mensagem;
+
+    @BeforeEach
+    public void setUp() {
+        carro = new Carro("RENAULT", "CLIO", 100, 200, 100, 12);
+    }
 
     @Test
     void testPrecisaCalibrarPneus() {
-        Pneu p = new Pneu(70);
-        assertTrue(p.precisaCalibrarPneus());
+        Pneu p1 = new Pneu(70);
+        assertTrue(p1.precisaCalibrarPneus());
+
         Pneu p2 = new Pneu(71);
         assertFalse(p2.precisaCalibrarPneus());
+    }
+
+    @Test
+    void testGirarPneuSemCinto() {
+        mensagem = carro.getPneu().girarPneus(carro.getBanco());
+        assertEquals("Por favor coloque o cinto!", mensagem);
+    }
+
+    @Test
+    void testGirarPneuComCinto() {
+        carro.getBanco().colocarCinto();
+        mensagem = carro.getPneu().girarPneus(carro.getBanco());
+        assertEquals("Pneus girando.", mensagem);
     }
 }
