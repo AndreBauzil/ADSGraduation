a26d9ede6ef928fab2daa7a5109693546e7e7dcb
AndreBauzil <89871705+AndreBauzil@users.noreply.github.com>
2023-10-26 15:04:21 +0000

    Ajuste do primeiro exercício da atividade 6 de Prog. Web 1, onde havia que editar elementos da página  conforme o botão e enunciado específicados, por exemplo atribuir inputs à variáveis, mostrar alerts ou mudar estilo de certos elementos da pagina

--- 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
+++ 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
@@ -6,51 +6,41 @@ btns[1].onmouseover = () => alert("Mouse passado sobre o segundo botão")
 btns[2].onmouseout = () => alert("Mouse foi retirado do terceiro botão")
 
 // Exarcício 2
-let nome = document.querySelector("#paragrafoExercicio2 > span")
 const inputs = document.querySelectorAll("input")
 
-let nomeDigitado
-
 btns[3].onclick = () => {
-    nomeDigitado = inputs[0].value
-    console.log(nomeDigitado)
-    nome.innerHTML= nomeDigitado
+    document.querySelector("#paragrafoExercicio2 > span").innerHTML= inputs[0].value
 }
 
 // Exercício 3
-let lista = document.getElementById("listaNumeros")
-
 btns[4].onclick = () => {
     let numero = inputs[1].value;
     for (let i = numero; i > 1; i--) {
         debugger
-        lista.innerHTML += numero + ", "
-        console.log(numero + ",");
+        document.getElementById("listaNumeros").innerHTML += numero + ", "
     }
 }
 
 // Exercício 4
 const paragrafo = document.getElementById("paragrafoExercicio4")
 
-
-
 btns[5].onclick = () => {
-    btns.indexOf(id = "exercicio4btn1")
+    btns.indexOf(id = "exercicio4btn1") // Encontra o index do botão do exercício 4
     debugger
     btns.forEach((btn, index) => {
         debugger
         switch (index) {
             case 5:
-                paragrafo.style.color = "blue"
+                paragrafo.style.color = "blue";
                 break;
             case 6:
                 paragrafo.style.color = "red"
                 break;
             case 7:
-                paragrafo.style.color = "black"
+                paragrafo.style.backgroundColor = "black"
                 break;
             case 8: 
-                paragrafo.style.color = "grey"
+                paragrafo.style.backgroundColor = "grey"
                 break;
             case 9:
                 paragrafo.style.fontSize = "20px"
