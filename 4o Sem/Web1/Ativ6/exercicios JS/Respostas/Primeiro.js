6fe72184c49f8e752cbfb7e1c25cca3ae89c2656
Andre Bauzil <andregbauzil@gmail.com>
2023-10-26 19:09:35 -0300

    Término do exercício 4 da primeira página da atividade 6 de Prog. Web 1, onde havia que mudar a caixa de texto conforme o botão clicado e especificação, utilizando switch

--- 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
+++ 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
@@ -1,50 +1,48 @@
 // Exercícios 1
-const btns = document.querySelectorAll("button")
+const btns = document.querySelectorAll("button") // Pega todos os botões
 
 btns[0].onclick = () => alert("Primeiro botão foi clicado")
 btns[1].onmouseover = () => alert("Mouse passado sobre o segundo botão")
 btns[2].onmouseout = () => alert("Mouse foi retirado do terceiro botão")
 
 // Exarcício 2
-const inputs = document.querySelectorAll("input")
+const inputs = document.querySelectorAll("input") // Pega todos os inputs
 
-btns[3].onclick = () => {
-    document.querySelector("#paragrafoExercicio2 > span").innerHTML= inputs[0].value
-}
+    // Adiciona o valor do input no html
+btns[3].onclick = () => document.querySelector("#paragrafoExercicio2 > span").innerHTML = inputs[0].value
 
 // Exercício 3
 btns[4].onclick = () => {
-    let numero = inputs[1].value;
-    for (let i = numero; i > 1; i--) {
-        debugger
-        document.getElementById("listaNumeros").innerHTML += numero + ", "
-    }
+    document.getElementById("listaNumeros").innerHTML = ''  // Limpa a lista
+    for (let i = inputs[1].value; i > 1; i--) 
+        document.getElementById("listaNumeros").innerHTML += i-1 + (i == 2 ? "." : ", ")  // Adiciona o .value do input - 1 + ', ' caso o número não seja 2 (último), se for, add '. '
 }
 
 // Exercício 4
 const paragrafo = document.getElementById("paragrafoExercicio4")
 
-btns[5].onclick = () => {
-    btns.indexOf(id = "exercicio4btn1") // Encontra o index do botão do exercício 4
-    debugger
-    btns.forEach((btn, index) => {
-        debugger
+btns.forEach((btn,index) => {
+    btn.onclick = () => {
         switch (index) {
             case 5:
-                paragrafo.style.color = "blue";
+                paragrafo.style.color = 'blue';
                 break;
             case 6:
-                paragrafo.style.color = "red"
+                paragrafo.style.color = 'red';
                 break;
             case 7:
-                paragrafo.style.backgroundColor = "black"
+                paragrafo.style.backgroundColor = 'black';
                 break;
-            case 8: 
-                paragrafo.style.backgroundColor = "grey"
+            case 8:
+                paragrafo.style.backgroundColor = 'grey';
                 break;
             case 9:
-                paragrafo.style.fontSize = "20px"
+                paragrafo.style.fontSize = '20px';
                 break;
+            case 10:
+                paragrafo.style.color = 'black';
+                paragrafo.style.fontSize = ''
+                paragrafo.style.backgroundColor = ''
         }
-    })
-}
+    }
+})
