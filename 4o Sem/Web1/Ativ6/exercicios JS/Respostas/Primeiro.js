19b170557dd0ad27381295736f1ba03362732776
Andre Bauzil <andregbauzil@gmail.com>
2023-11-17 11:46:05 -0300

    Commit para limpeza da fila de arquivos faltantes, Ativ2 de Mobile, Ativ6 de Web1

--- 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
+++ 4o Sem/Web1/Ativ6/exercicios JS/Respostas/Primeiro.js
@@ -9,7 +9,7 @@ btns[2].onmouseout = () => alert("Mouse foi retirado do terceiro botão")
 const inputs = document.querySelectorAll("input")
 
 btns[3].onclick = () => {
-    document.querySelector("#paragrafoExercicio2 > span").innerHTML= inputs[0].value
+    document.querySelector("#paragrafoExercicio2 > span").innerHTML = inputs[0].value
 }
 
 // Exercício 3
@@ -41,6 +41,61 @@ btns[5].onclick = () => {
                 break;
             case 8: 
                 paragrafo.style.backgroundColor = "grey"
+                break;
+            case 9:
+                paragrafo.style.fontSize = "20px"
+                break;
+        }
+    })
+}
+
+
+// Exarcício 2
+let nome = document.querySelector("#paragrafoExercicio2 > span")
+const inputs = document.querySelectorAll("input")
+
+let nomeDigitado
+
+btns[3].onclick = () => {
+    nomeDigitado = inputs[0].value
+    console.log(nomeDigitado)
+    nome.innerHTML= nomeDigitado
+}
+
+// Exercício 3
+let lista = document.getElementById("listaNumeros")
+
+btns[4].onclick = () => {
+    let numero = inputs[1].value;
+    for (let i = numero; i > 1; i--) {
+        debugger
+        lista.innerHTML += numero + ", "
+        console.log(numero + ",");
+    }
+}
+
+// Exercício 4
+const paragrafo = document.getElementById("paragrafoExercicio4")
+
+
+
+btns[5].onclick = () => {
+    btns.indexOf(id = "exercicio4btn1")
+    debugger
+    btns.forEach((btn, index) => {
+        debugger
+        switch (index) {
+            case 5:
+                paragrafo.style.color = "blue"
+                break;
+            case 6:
+                paragrafo.style.color = "red"
+                break;
+            case 7:
+                paragrafo.style.color = "black"
+                break;
+            case 8: 
+                paragrafo.style.color = "grey"
                 break;
             case 9:
                 paragrafo.style.fontSize = "20px"
