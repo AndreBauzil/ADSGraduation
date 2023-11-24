80f474eec11af662ae9109f848c1adb1507a95d8
Andre Bauzil <andregbauzil@gmail.com>
2023-11-23 23:29:26 -0300

    Script para criação do carrossel de depoimentos

---
+++ 4o Sem/Web1/Ativ8/assets/js/depoimentosCarousel.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#myCarousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        nav: false, // Enable navigation arrows
+        dots: true, // Enable dots navigation
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            }
+        }
+    });
+});
