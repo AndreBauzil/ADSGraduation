1179d2ca79c32161b260b2a2edf4eee1ba5ca786
Andre Bauzil <andregbauzil@gmail.com>
2023-11-27 18:01:22 -0300

    Ajuste na data da função do contador de dias

--- 4o Sem/Web1/Ativ8/assets/js/contador.js
+++ 4o Sem/Web1/Ativ8/assets/js/contador.js
@@ -2,10 +2,10 @@ let result = document.getElementById("contador")
 
 setTimeout(() => {
     let hoje = new Date()
-    let data = new Date(2024, 11, 30)
+    let data = new Date(2024, 10, 30)
     
     let diffTempo = data - hoje
     let diffDias = Math.ceil(diffTempo / (1000 * 60 * 60 * 24))
     
-    result.innerHTML = "Faltam " + diffDias + " dias"
+    result.innerHTML = "Faltam " + (diffDias - 1)  + " dias"
 }, 1000);
