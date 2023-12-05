61d1027197c481b196c770c3e31f8bb854832bc5
Andre Bauzil <andregbauzil@gmail.com>
2023-12-05 14:42:00 -0300

    Criação e começo da estrutura da atividade 9 de Prog. Web1 usando React

---
+++ 4o Sem/Web1/ativ9/src/App.js
@@ -0,0 +1,19 @@
+import { Primeiro } from "./components/primeiro"
+import "./style.css"
+
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+// import { Home } from "./pages/primeiro";
+// import { Login } from "./pages/segundo";
+
+export default function App() {
+  return (
+    <div className="Primeiro App">
+     <Primeiro/>
+    </div>
+  )
+}
