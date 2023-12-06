561ebdf04adcedb6b630fe5577605f3971f88151
Andre Bauzil <andregbauzil@gmail.com>
2023-12-06 19:17:21 -0300

    Começo da estruturação do exercício 2, criação automática de posts

--- Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
+++ Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
 
-const Post = ({ src, data, desc, nome }) => {
-  <div>
+const Post = ({ src, data, desc, nome }) => (
+  <div className="post">
     <img src={src} alt="Imagem auto-gerada" />
     <p>Data: {data}</p>
     <p>Desrição: {desc}</p>
     <p>Nome: {nome}</p>
   </div>
-};
+);
 
 
 export const Segundo = () => {
-  const posts = [
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-    { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  ]
+  const [components, setComponents] = useState([])
+
+  // const posts = [
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
+  // ]
+
+  const criarPost = () => {
+    setComponents([...components, "Teste"]) // Seta um novo post junto com os já existentes para persistência dos dados
+  }
 
   return (
-    <div>
-      {posts.map(post => {
-        <Post {...post} />
-      })}
-    </div>
+    <>
+     <button onClick={ criarPost }>Criar Posts</button> 
+     <div className="posts">
+      { components.map((post, i) => ( <Post key={i} {...post} />)) }
+     </div>
+    </>
   )
 }
