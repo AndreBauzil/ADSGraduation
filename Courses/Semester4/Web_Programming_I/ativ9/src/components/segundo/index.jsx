b5621c0284813dc3dee20e672ac44c5e64f7aff6
Andre Bauzil <andregbauzil@gmail.com>
2023-12-07 11:54:29 -0300

    Término do exercício 2, gerando automaticamente post

--- Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
+++ Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
@@ -1,27 +1,38 @@
 import { useState } from "react";
 
-const Post = ({ src, data, desc, nome }) => (
+
+export const Post = ({ data, desc, nome }) => (
   <div className="post">
-    <img src={src} alt="Imagem auto-gerada" />
-    <p>Data: {data}</p>
-    <p>Desrição: {desc}</p>
-    <p>Nome: {nome}</p>
+      <p>Data: {data}</p>
+      <p>Desrição: {desc}</p>
+      <p>Nome: {nome}</p>
   </div>
 );
 
-
 export const Segundo = () => {
   const [components, setComponents] = useState([])
+
+  // variáveis usadas na geração automática
   let lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
   const nomes = ['Ana', 'João', 'Maria', 'Pedro', 'Carla', 'Luís', 'Mariana', 'Rafael'];
   const sobrenomes = ['Silva', 'Santos', 'Oliveira', 'Pereira', 'Costa', 'Martins', 'Ferreira', 'Almeida'];
-  let random = Math.random()*nomes.length
-
-
-  const criarPost = () => {
-    setComponents([...components, "Teste"]) // Seta um novo post junto com os já existentes para persistência dos dados
-    debugger
+  
+  // funções
+  const criarPost = () =>{
+    const novoPost = {
+      data: new Date(
+        2023, 
+        numRandom(12),  // mês
+        numRandom(30)   // dia
+      ).toLocaleDateString("en-GB"),
+      desc: lorem,
+      nome: nomes[numRandom(nomes.length)] + " " + sobrenomes[numRandom(sobrenomes.length)]
+    }
+    setComponents([...components, novoPost])
   }
+  // const criarPost = () => setComponents([...components, "Teste"])   // Seta um novo post junto com os já existentes para persistência dos dados
+  const numRandom = max => Math.floor(Math.random()*max)  // Geração de número random, usado diversas vezes
+
 
   return (
     <>
@@ -31,9 +42,19 @@ export const Segundo = () => {
         <Post 
           key={i} 
           {...post} 
-          data={Date.now+i} 
-          desc={ lorem } 
-          nome={nomes[random] + " " + sobrenomes[random]} 
+          src={`../../assets/imgs/${numRandom(10)}.png`}
+          // data={
+          //   new Date(
+          //     2023, 
+          //     numRandom(12),  // mês
+          //     numRandom(30)   // dia
+          //   ).toLocaleDateString("en-GB")
+          // } 
+          // desc={ lorem } 
+          // nome={
+          //   nomes[numRandom(nomes.length)] + " " + 
+          //   sobrenomes[numRandom(sobrenomes.length)]
+          // } 
         /> 
       ))}
      </div>
