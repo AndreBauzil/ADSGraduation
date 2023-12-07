550fb7b1a3a921b6f235266427dd06faee1e7478
AndreBauzil <Andregbauzil@gmail.com>
2023-12-07 02:22:27 -0300

    Adição de conteúdo para geração aleatória dos post

--- Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
+++ Courses/Semester4/Web_Programming_I/ativ9/src/components/segundo/index.jsx
@@ -12,26 +12,30 @@ const Post = ({ src, data, desc, nome }) => (
 
 export const Segundo = () => {
   const [components, setComponents] = useState([])
+  let lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  const nomes = ['Ana', 'João', 'Maria', 'Pedro', 'Carla', 'Luís', 'Mariana', 'Rafael'];
+  const sobrenomes = ['Silva', 'Santos', 'Oliveira', 'Pereira', 'Costa', 'Martins', 'Ferreira', 'Almeida'];
+  let random = Math.random()*nomes.length
 
-  // const posts = [
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  //   { src: 'url1', data: '01/01/2023', descricao: 'Descrição 1', nome: 'Nome 1' },
-  // ]
 
   const criarPost = () => {
     setComponents([...components, "Teste"]) // Seta um novo post junto com os já existentes para persistência dos dados
+    debugger
   }
 
   return (
     <>
      <button onClick={ criarPost }>Criar Posts</button> 
      <div className="posts">
-      { components.map((post, i) => ( <Post key={i} {...post} />)) }
+      { components.map((post, i) => (
+        <Post 
+          key={i} 
+          {...post} 
+          data={Date.now+i} 
+          desc={ lorem } 
+          nome={nomes[random] + " " + sobrenomes[random]} 
+        /> 
+      ))}
      </div>
     </>
   )
