991ca7430e7f140f8f793e8c509aaf50e521d2ea
AndreBauzil <Andregbauzil@gmail.com>
2023-12-14 00:45:48 -0300

    Pequenas correções de bugs e mudanças

--- Courses/Semester4/Mobile_Programming_I/AtivFinalMobile/app/src/main/java/com/bauzil/ativfinalmobile/ui/theme/MainActivity.kt
+++ Courses/Semester4/Mobile_Programming_I/AtivFinalMobile/app/src/main/java/com/bauzil/ativfinalmobile/ui/theme/MainActivity.kt
@@ -8,22 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.bauzil.ativfinalmobile.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener,
-    View.OnFocusChangeListener {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.toolbar)
-
-        setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         val searchView = menu.findItem(R.id.action_search).actionView as SearchView
-        searchView.setOnQueryTextFocusChangeListener(this)
+        searchView.setOnQueryTextListener(this)
         return super.onCreateOptionsMenu(menu)
     }
 
