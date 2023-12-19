8dbd741ed50645ea1f1bbe2610c76e1675140578
Andre Bauzil <andregbauzil@gmail.com>
2023-12-19 12:28:19 -0300

    Ajustes no exercício um para remover placeholder quando clicado, além de remoção do label

--- Courses/Semester4/Web_Programming_I/ativ10/src/components/primeiro/index.jsx
+++ Courses/Semester4/Web_Programming_I/ativ10/src/components/primeiro/index.jsx
@@ -1,59 +1,72 @@
 import React, { useState } from 'react';
-import { Escolha, Paragrafo } from './style';  // Estilização do parágrafo
+import { Escolha, Paragrafo, Label, Input, onClick, onBlur } from './style';  // Estilização do parágrafo
 
 
 export const Primeiro = () => {
-  const [pergunta, setPergunta] = useState('Faça uma pergunta');
-  const [opcao1, setOpcao1] = useState('');
-  const [opcao2, setOpcao2] = useState('');
+  // Boolean enviada ou não
+  const [enqueteEnviada, setEnqueteEnviada] = useState(false);
+  // Inputs
+  const [pergunta, setPergunta] = useState('Escreva uma pergunta');
+  const [opcao1, setOpcao1] = useState('Primeira Resposta');
+  const [opcao2, setOpcao2] = useState('Segunda Resposta');
+  // Contador
   const [votosOpcao1, setVotosOpcao1] = useState(0);
   const [votosOpcao2, setVotosOpcao2] = useState(0);
-  const [enqueteEnviada, setEnqueteEnviada] = useState(false);
 
   return (
     <>
-      {enqueteEnviada ? (
+      {!enqueteEnviada ? (
         <>
+          <Label>
+            <Input
+              value={pergunta}
+              onClick={onClick(setPergunta, '')}
+              onBlur={onBlur(setPergunta, 'Escreva uma pergunta')}
+              onChange={e => setPergunta(e.target.value)}
+            />
+          </Label>
+
+          <div className="opcoes">
+            <Label>
+              <Input
+                value={opcao1}
+                onClick={onClick(setOpcao1, '')}
+                onBlur={onBlur(setOpcao1, 'Primeira Resposta')}
+                onChange={e => setOpcao1(e.target.value)}
+              />
+            </Label>
+            <Label>
+              <Input
+                value={opcao2}
+                onClick={onClick(setOpcao2, '')}
+                onBlur={onBlur(setOpcao2, 'Segunda Resposta')}
+                onChange={e => setOpcao2(e.target.value)}
+              />
+            </Label>
+          </div>
+            <button onClick={() => setEnqueteEnviada(true)}>Enviar</button>
+      </>
+      ) : (
+       <>
           <Paragrafo>{pergunta}</Paragrafo>
 
           <div className="votos">
             <Escolha>
-                <Paragrafo>{opcao1}</Paragrafo> 
-                <button onClick={() => setVotosOpcao1(votosOpcao1 + 1)}>{votosOpcao1} votos</button>
+              <Paragrafo>{opcao1}</Paragrafo> 
+              <button onClick={() => setVotosOpcao1(votosOpcao1 + 1)}>
+                {votosOpcao1} votos
+              </button>
             </Escolha>
-            <Escolha>
-                <Paragrafo>{opcao2}</Paragrafo> 
-                <button onClick={() => setVotosOpcao2(votosOpcao2 + 1)}>{votosOpcao2} votos</button>
-            </Escolha>
-          </div>
-        </>
-      ) : (
-        <>
-            <label>Pergunta:
-                <input
-                    type="text"
-                    value={pergunta}
-                    onChange={e => setPergunta(e.target.value)}
-                />
-            </label>
 
-            <div className="opcoes">
-                <label>Opção 1:
-                    <input
-                        type="text"
-                        value={opcao1}
-                        onChange={e => setOpcao1(e.target.value)}
-                    />
-                </label>
-                <label>Opção 2:
-                    <input
-                        type="text"
-                        value={opcao2}
-                        onChange={e => setOpcao2(e.target.value)}
-                    />
-                </label>
-                <button onClick={() => setEnqueteEnviada(true)}>Enviar</button>
-            </div>
+            <button onClick={() => setEnqueteEnviada(false)}>↺</button>
+            <Escolha>
+              <Paragrafo>{opcao2}</Paragrafo> 
+              <button onClick={() => setVotosOpcao2(votosOpcao2 + 1)}>
+                {votosOpcao2} votos
+              </button>
+            </Escolha>
+
+          </div>
         </>
       )}
     </>
