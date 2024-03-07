8918eecd99fa98a987e95556a550ac0437b3b9a5
Andre Bauzil <andregbauzil@gmail.com>
2024-03-06 21:20:04 -0300

    feat: add a new activity to create a CRUD using SpringBoot

---
+++ Courses/Semester5/Web_Programming_II/web2/src/main/java/com/web2/repositories/ProdutosRepository.java
@@ -0,0 +1,12 @@
+package com.web2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web2.model.ProdutosModel;
+
+@Repository
+public interface ProdutosRepository 
+    extends JpaRepository<ProdutosModel, Integer> {
+    
+}
