13c12e4aecc00e7cad1f95d2bcd10848097c9f99
AndreBauzil <Andregbauzil@gmail.com>
2024-03-27 20:03:33 -0300

    feat: add modelMapper config file to map list of objects into DTO objects

---
+++ Courses/Semester5/Web_Programming_II/courses_api/src/main/java/com/bauzil/courses_api/config/Config.java
@@ -0,0 +1,14 @@
+package com.bauzil.courses_api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
