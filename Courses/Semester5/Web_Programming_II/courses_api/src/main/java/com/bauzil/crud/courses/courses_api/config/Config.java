3fba06b039381a39bd2db353b23bd1bd32a61cd9
AndreBauzil <Andregbauzil@gmail.com>
2024-03-27 01:19:05 -0300

    chore: add configuration file

---
+++ Courses/Semester5/Web_Programming_II/courses_api/src/main/java/com/bauzil/crud/courses/courses_api/config/Config.java
@@ -0,0 +1,14 @@
+package com.bauzil.crud.courses.courses_api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
