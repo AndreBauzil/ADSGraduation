2e0cda7fe93ca8e81a1f8b7e4dcf9588fda2b0cb
AndreBauzil <Andregbauzil@gmail.com>
2024-03-27 20:08:29 -0300

    refactor: change directory structure to better understanding

--- Courses/Semester5/Web_Programming_II/courses_api/src/main/java/com/bauzil/crud/courses/courses_api/repositories/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bauzil.crud.courses.courses_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.bauzil.crud.courses.courses_api.model.Curso;
-
-@Repository
-public interface CursoRepository extends JpaRepository<Curso, Long> {}
